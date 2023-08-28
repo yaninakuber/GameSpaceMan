15bedbf8d497ec5ab24a3776bbade218fd5eaecc
Yanina Kuber <yaninakuber@gmail.com>
2023-08-28 14:01:56 -0300

    Refact CameraFollow

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target; // declaro variable que siga al objetivo
+    public Transform target; 
     public Vector3 offset = new Vector3(-3f, 0.0f, -10f); // a que distancia debe seguir al personaje
     public float dampingTime = 0.3f; // tiempo de amortiguacion para que no sea un choque muy bruzco, la cam tardara un poco en seguir, efecto cine 
-    public Vector3 velocityCam = Vector3.zero; //velocidad que va la camara, inicializamos con cero
+    public Vector3 velocityCam = Vector3.zero; 
 
-    private bool isResettingCamera = false; // Variable para controlar si la cámara se está reiniciando
+    private bool isResettingCamera = false; 
     private bool isFirstMove = true; // arregla bugg de movimiento cortado la primera vez
 
 
@@ -17,53 +17,80 @@ public class CameraFollow : MonoBehaviour
     {
         Application.targetFrameRate = 60;  // va a intentar ir a 60 frame x seg
         MoveCamera(true); // inicializa el movimiento suave
-
     }
 
-
-    void Start()
+    private void Update()
     {
+        GameState gameState = GameManager.sharedInstance.currentGameState;
 
-    }
-
-
-    void Update()
-    {
-        if (GameManager.sharedInstance.currentGameState == GameState.inGame || GameManager.sharedInstance.currentGameState == GameState.menu)
+        if(IsInGameOrMenuState(gameState))
         {
-            if (isFirstMove) {
-                isFirstMove = false;
-                MoveCamera(true);
-            } else {
-                MoveCamera(true);
-                isResettingCamera = false;
-            }
-        } else if(GameManager.sharedInstance.currentGameState == GameState.gameOver && !isResettingCamera || GameManager.sharedInstance.currentGameState == GameState.win && !isResettingCamera) {
-            Invoke("ResetCameraPosition", 0.5f); // Llamar al método ResetCameraPosition con un retraso de 2 segundos;
+            HandleInGameOrMenuState();
+        } 
+        else if (IsGameOverOrWinState(gameState)) 
+        {
+            HandleGameOverOrWinState();
+        }
+    }
+
+    private bool IsInGameOrMenuState(GameState state)
+    {
+        return state == GameState.inGame || state == GameState.menu;
+    }
+    private bool IsGameOverOrWinState(GameState state)
+    {
+        return state == GameState.gameOver || state == GameState.win;
+    }
+
+    void HandleInGameOrMenuState()
+    {
+        if (isFirstMove)
+        {
+            isFirstMove = false;
+            MoveCamera(true);
+        }
+        else
+        {
+            MoveCamera(true);
+            isResettingCamera = false;
+        }
+    }
+
+    void HandleGameOverOrWinState()
+    {
+        if (!isResettingCamera)
+        {
+            Invoke("ResetCameraPosition", 0.5f);
             isResettingCamera = true;
         }
     }
 
-    public void MoveCamera(bool smooth)
+    public void SmoothMoveCamera(bool smooth)
     {
-        Vector3 destinationInGame = new Vector3(Mathf.Max(target.position.x - offset.x, this.transform.position.x), offset.y, offset.x); // destino que restringe mov de cam a la izquierda
-        Vector3 destinationReset = new Vector3(offset.x, offset.y, offset.z); // destino que deja quieta la camara
-
-        if (smooth) {
-            this.transform.position = Vector3.SmoothDamp( // lo sigue lentamente 
-                            this.transform.position,
-                            destinationInGame,
-                            ref velocityCam,
-                            dampingTime);
-        }
-       else
-       {
-           this.transform.position = destinationReset;
-       }
-
+        Vector3 destination = smooth ? CalculateSmoothDestination() : CalculateResetDestination();
+        this.transform.position = destination;
     }
 
-    public void ResetCameraPosition() {
+    Vector3 CalculateSmoothDestination()
+    {
+        Vector3 targetPosition = target.position;
+        Vector3 restrictedPosition = new Vector3(Mathf.Max(targetPosition.x - offset.x, this.transform.position.x), offset.y, offset.z); // destino que restringe mov de cam a la izquierda
+        return Vector3.SmoothDamp(this.transform.position, restrictedPosition, ref velocityCam, dampingTime); // lo sigue lentamente 
+    }
+
+    private void ResetCameraPosition()
+    {
         MoveCamera(false);
+    }
+
+    private void MoveCamera(bool shouldSmooth)
+    {
+        Vector3 destination = shouldSmooth ? CalculateSmoothDestination() : CalculateResetDestination();
+        this.transform.position = destination;
+    }
+
+    private Vector3 CalculateResetDestination()
+    {
+        return new Vector3(offset.x, offset.y, offset.z);
     }
 }
